23f4262e687c061762ef8489a44ce1b448d70d5c
Markus Reynolds <markus.reynolds1989@gmail.com>
2019-12-31 13:55:10 -0600

    First

---
+++ InternalWiki/Data/Article.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternalWiki.Data
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public IList<string> Tags { get; set; }
+        public string Content { get; set; }
+        
+    }
+}
---
+++ InternalWiki/Pages/New.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using InternalWiki.Data;
+using System.IO;
+using System.Text;
+
+namespace InternalWiki
+{
+    public class NewModel : PageModel
+    {
+        [BindProperty]
+        public Article Article { get; set; } 
+           
+        public void OnPost()
+        {
+            var tags = new StringBuilder();
+            foreach(var item in Article.Tags)
+            {
+                tags.Append(item);
+            }
+            var saveString =
+                "@page \n@{ViewData[\"Title\"] =" +
+                $"\"{Article.Title}\";" + 
+                "}" +
+                $@"<h1> {Article.Title} </h1>" +
+                $@"<p> {Article.Content} </p>";
+            var path = $"Pages/{Article.Title}.cshtml";
+            System.IO.File.WriteAllText(path, saveString);
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
